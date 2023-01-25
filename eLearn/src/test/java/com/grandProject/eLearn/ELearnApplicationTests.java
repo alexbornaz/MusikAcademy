649b02debdb6cb6172d4c79965b38cf7f1101ffd
alex <bornazalexg@gmail.com>
2023-01-25 14:15:53 +0200

    Spring boot project initialized

---
+++ eLearn/src/test/java/com/grandProject/eLearn/ELearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandProject.eLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
