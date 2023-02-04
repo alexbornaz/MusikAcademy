688d47977b9c362d09d89a23356d707c17b44df6
alex <bornazalexg@gmail.com>
2023-02-04 13:14:48 +0200

    created carousel for home page added images

---
+++ elearn/src/components/Carousel/Carousel.js
@@ -0,0 +1,65 @@
+import carouselImage1 from "../images/1.jpg";
+import c2 from "../images/2.jpg";
+import classes from "./Carousel.module.css";
+const Carousel = () => {
+  return (
+        <div
+          id="HomeCarousel"
+          className="carousel slide carousel-fade"
+          data-bs-ride="true"
+        >
+          <div className="carousel-inner">
+            <div className={`carousel-item active ${classes.cItem}`}>
+              <img
+                src={carouselImage1}
+                className={`d-block w-100 ${classes.cImg}`}
+                alt="..."
+              />
+              <div
+                className={`carousel-caption d-none d-md-block ${classes.captionTitle}`}
+              >
+                <h3>Learn to play</h3>
+              </div>
+            </div>
+            <div className={`carousel-item ${classes.cItem}`}>
+              <img
+                src={c2}
+                className={`d-block w-100 ${classes.cImg}`}
+                alt="..."
+              />
+              <div
+                className={`carousel-caption d-none d-md-block ${classes.captionTitle}`}
+              >
+                <h3>Teach to play</h3>
+              </div>
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#HomeCarousel"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className={`carousel-control-next ${classes.arrowColor}`}
+            type="button"
+            data-bs-target="#HomeCarousel"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+  );
+};
+export default Carousel;
