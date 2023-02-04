cab14b269c05ac3ce5d80eb17d36995497624eaa
alex <bornazalexg@gmail.com>
2023-02-04 16:04:03 +0200

    filter chain method to allow any request for test purposes

---
+++ eLearn/src/main/java/com/grandProject/eLearn/securityConfig/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.grandProject.eLearn.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
+    }
+
+}
+
