01de95400fa52caa9bbe0f936666b66b4bb57a3e
alex <bornazalexg@gmail.com>
2023-02-04 16:03:31 +0200

    user repository interface created and methods needed implemented

---
+++ eLearn/src/main/java/com/grandProject/eLearn/service/user/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.grandProject.eLearn.service.user;
+
+import com.grandProject.eLearn.model.user.User;
+import com.grandProject.eLearn.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public boolean hasUserWithUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean hasUserWithEmail(String email) {
+        return userRepository.existsByEmailAddress(email);
+    }
+
+    @Override
+    public User validateAndGetUserByUsername(String username) {
+        return getUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(String.format("User with username %s not found", username)));
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+}
