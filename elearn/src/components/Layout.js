2d6c0d8574e36849f6d37ce67d78a4a60038dc14
alex <bornazalexg@gmail.com>
2023-02-04 13:16:44 +0200

    added authentication buttons to navbar

--- elearn/src/components/Layout.js
+++ elearn/src/components/Layout.js
@@ -1,27 +1,71 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-function Layout({ children }) {
+import Footer from "./Footer";
+import logo from "./images/MusikAcademyLogo.png";
+
+const Layout = ({ children }) => {
+  const [isDark, setDark] = useState(true);
+  const toggleDark = () => {
+    setDark(!isDark);
+    console.log(isDark);
+    isDark
+      ? (document.body.dataset.bsTheme = "dark")
+      : (document.body.dataset.bsTheme = "light");
+  };
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
+            <li>
+              <Link className="navbar-brand" to="/">
+                <img
+                  src={logo}
+                  className="d-inline-block align-text-center"
+                  alt="logo"
+                  width="50"
+                />
+                MusikAcademy
               </Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/courses">
-                Courses
+                All Courses
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/report">
+                Report an issue
               </Link>
             </li>
           </ul>
         </div>
+        <Link to="/register" className="btn  btn-block">
+          SignUp
+        </Link>
+        <Link to="/login" className="btn  mx-2 btn-block">
+          Login
+        </Link>
+        <div>
+          <button type="button" onClick={toggleDark}>
+            {isDark ? (
+              <i className="fas fa-moon"></i>
+            ) : (
+              <i className="fas fa-lightbulb"></i>
+            )}
+          </button>
+        </div>
       </nav>
       <main>{children}</main>
       <Outlet />
+      <Footer />
     </>
   );
-}
+};
 
 export default Layout;
