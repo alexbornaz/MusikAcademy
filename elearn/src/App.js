f673ab373c652bb2eb2416c1cd5879f8fa84d9b4
alex <bornazalexg@gmail.com>
2023-02-04 13:17:45 +0200

    routes added

--- elearn/src/App.js
+++ elearn/src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./components/Home";
-import Courses from "./components/Courses";
+import Home from "./components/pages/Home";
+import Courses from "./components/pages/Courses";
+import About from "./components/pages/About";
+import Report from "./components/pages/Report";
+import Login from "./components/pages/login/Login";
+import Registration from "./components/pages/register/Registration";
+import Course from "./components/pages/Course";
 
 function App() {
   return (
@@ -10,8 +15,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route exact path="courses" element={<Courses />} />
+          <Route path="courses">
+            <Route index element={<Courses />} />
+            <Route path="course/:id" element={<Course />} />
+          </Route>
+          <Route exact path="about" element={<About />} />
+          <Route exact path="report" element={<Report />} />
         </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Registration />} />
       </Routes>
     </BrowserRouter>
   );
