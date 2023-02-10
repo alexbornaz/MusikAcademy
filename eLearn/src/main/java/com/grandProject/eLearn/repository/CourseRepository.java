4ebaaf8914b611e4bbb11e2bf594c7c310a2bc99
alex <bornazalexg@gmail.com>
2023-02-10 14:50:30 +0200

    update

--- eLearn/src/main/java/com/grandProject/eLearn/repository/CourseRepository.java
+++ eLearn/src/main/java/com/grandProject/eLearn/repository/CourseRepository.java
@@ -2,6 +2,9 @@ package com.grandProject.eLearn.repository;
 
 import com.grandProject.eLearn.model.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course,Long> {
     boolean existsById(Long id);
